2cbd2fc0647f2b89afe086b89b2986705d7f7568
Vinni949 <83116455+Vinni949@users.noreply.github.com>
2023-02-21 11:09:16 +0300

    AddDBClass&ProductClass

---
+++ TestTask210223/Models/DBProduct.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TestTask210223.Models
+{
+    public class DBProduct:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DBProduct(DbContextOptions<DBProduct> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
